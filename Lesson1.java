452dfad15e392fb37d12ad5b27870929618bcc36
Natnael <natnaelebuy@gmail.com>
2021-10-19 14:59:11 -0700

    update vaccination name

--- Lesson1.java
+++ Lesson1.java
@@ -5,21 +5,21 @@ public class Lesson1 {
     
    public static void main(String[] args) {
     HashMap<String, String> vaccination = new HashMap<>();
-    vaccination.put("Natnael", "12/07/1998");
-    vaccination.put("Yordanos", "06/20/1980");
-    vaccination.put("Yonatan", "11/07/2013");
-    vaccination.put("Eden", "06/18/2000");
-    vaccination.put("Hawaz", "03/02/1993");
-    vaccination.put("Mekdess", "10/13/1981");
-    vaccination.put("Gelila", "12/31/2018");
-    vaccination.put("Jose", "09/02/1990");
-    vaccination.put("Abele", "10/18/1998");
+    vaccination.put("Natnael", "pfizer-alpha");
+    vaccination.put("Yordanos", "moderna-alpha");
+    vaccination.put("Yonatan", "johnson-alpha");
+    vaccination.put("Eden", "pfizer-omega");
+    vaccination.put("Hawaz",  "pfizer-kappa");
+    vaccination.put("Mekdess", "moderna-delta");
+    vaccination.put("Gelila", "moderna-gamma");
+    vaccination.put("Jose", "moderna-zeta");
+    vaccination.put("Abele", "moderna-zeta");
 
     Scanner scanner = new Scanner(System.in);
             System.out.print("Enter your name: ");
             String name = scanner.nextLine();
             if(vaccination.get(name) != null)
-                System.out.println(name + " your birth date is: " + vaccination.get(name));
+                System.out.println(name + " your vaccination name is: " + vaccination.get(name));
             else
                 System.out.println("vaccination not found");
 
