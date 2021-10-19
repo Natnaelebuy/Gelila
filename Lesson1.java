3d1bf26a839546340ab3544abf90b7561da594e7
Natnael <natnaelebuy@gmail.com>
2021-10-19 15:35:10 -0700

    test

--- Lesson1.java
+++ Lesson1.java
@@ -1,33 +1,32 @@
+
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Lesson1 {
     
    public static void main(String[] args) {
-    HashMap<String, String> vaccination = new HashMap<>();
-    vaccination.put("Natnael", "pfizer-alpha");
-    vaccination.put("Yordanos", "moderna-alpha");
-    vaccination.put("Yonatan", "johnson-alpha");
-    vaccination.put("Eden", "pfizer-omega");
-    vaccination.put("Hawaz",  "pfizer-kappa");
-    vaccination.put("Mekdess", "moderna-delta");
-    vaccination.put("Gelila", "moderna-gamma");
-    vaccination.put("Jose", "moderna-zeta");
-    vaccination.put("Abele", "moderna-zeta");
+    int main()
+{
+// Declaring variables
+string firstName;
+int age;
 
-    Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter your name: ");
-            String name = scanner.nextLine();
-            if(vaccination.get(name) != null)
-                System.out.println(name + " your vaccination name is: " + vaccination.get(name));
-            else
-                System.out.println("vaccination not found");
+// Getting the firstname entered by the user
+cout << "Hawaz :";
+cin >> 25;
 
-       
-    
-    
+// Getting the age entered by the user
+cout << "Teacher :";
+cin >> 50;
 
-
+/* Based on age check the user is adult or
+* not by displaying corresponding message
+*/
+if (age >= 60)
+cout << firstName << " you are vaccinated" << endl;
+else
+cout << firstName << " you are not vaccinated" << endl;
+return 0;
 
 
         
