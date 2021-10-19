b7e45e7673f093eeccb1a2989d6e3fed82354123
Natnael <natnaelebuy@gmail.com>
2021-10-19 14:49:44 -0700

    update new data

--- Lesson1.java
+++ Lesson1.java
@@ -3,21 +3,21 @@ public class Lesson1 {
    public static void main(String[] args) {
        int ages[] = new int [7];
         
-       ages[0] = 22;
-       ages[1] = 24;
-       ages[2] = 23;
-       ages[3] = 26;
-       ages[4] = 25;
-       ages[5] = 22;
-       ages[6] = 24;
+       //ages[0] = 22;
+       //ages[1] = 24;
+       //ages[2] = 23;
+       //ages[3] = 26;
+       //ages[4] = 25;
+       //ages[5] = 22;
+       //ages[6] = 24;
 
-       System.out.println("Hawaz can get vaccinated pfizer-alpha because his age is " + ages[0]);
-       System.out.println("Gelila age is " + ages[1]);
-       System.out.println("Yonatan age is " + ages[2]);
-       System.out.println("Yordano age is " + ages[3]);
-       System.out.println("Mekdes age is " + ages[4]);
-       System.out.println("Natnael age is " + ages[5]);
-       System.out.println("Jose age is " + ages[6]);
+       //System.out.println("Hawaz can get vaccinated pfizer-alpha because his age is " + ages[0]);
+       //System.out.println("Gelila age is " + ages[1]);
+       //System.out.println("Yonatan age is " + ages[2]);
+       //System.out.println("Yordano age is " + ages[3]);
+       //System.out.println("Mekdes age is " + ages[4]);
+       //System.out.println("Natnael age is " + ages[5]);
+       //System.out.println("Jose age is " + ages[6]);
 
 
 
